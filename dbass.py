f0c5371e66963c4ca992735b332e75ae85230413
Hugues Fontenelle <hugues.fontenelle@gmail.com>
2014-10-13 21:25:01 +0200

    retrieve seq

--- dbass.py
+++ dbass.py
@@ -7,11 +7,13 @@ hugues.fontenelle@medisin.uio.no
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import urllib, re
+import urllib
 
-def parse_var(url):
-    fp2 = urllib.urlopen(url)
+# ------------------------------------------------
+def parse_var(varurl):
+    fp2 = urllib.urlopen(varurl)
     soup2 = BeautifulSoup(fp2)
+    global table2
     table2 = soup2.findAll('table')[0]
     tmp = table2.find(text='Gene').findNext('span')
     if tmp.find('abbr'):
@@ -28,32 +30,46 @@ def parse_var(url):
     mutation = table2.find(text='Mutation').findNext('td').string
     frameshift = table2.find(text='Change in the Reading Frame').findNext('td').div.string
     reference = table2.find(text='Reference(s)').findNext('td').div.p.a.previous
-    #print gene, mutation, exon, phenotype    
-    return [gene, mutation, exon, phenotype, dist, frameshift, reference]
+    div = table2.find('div', { 'id' : 'PageBody_pnlSequence' })
+    seq = []
+    #for span in div.descendants:
+    #    seq.append(span.string)
+    seq = div.findAll(text=True)
+    seq = [x for x in seq if x is not None]
+    seq = ''.join(seq).strip().replace(' ', '')
+    return [gene, mutation, exon, phenotype, dist, frameshift, reference, seq]
 
+# ------------------------------------------------
 def parse_page(soup):
     table = soup.findAll('table')[0]
     records = table.findAll('tr')
     page_db = []
-    for record in records[1:]:
-        td = record.findAll('td')
-        link = td[5].a['href']
-        varurl = "http://www.dbass.org.uk/DBASS5/" + link[2:]
-        variant = parse_var(varurl)
-        page_db.append(variant)
+    #for record in records[1:]:
+    record = records[1]
+    td = record.findAll('td')
+    link = td[5].a['href']
+    varurl = "http://www.dbass.org.uk/DBASS5/" + link[2:]
+    variant = parse_var(varurl)
+    page_db.append(variant)
     return page_db
-        
-if __name__ == "__main__":
-    db = []
+
+# ------------------------------------------------
+def parse_webpage():
     url = "http://www.dbass.org.uk/DBASS5/viewlist.aspx"
     next_page = 'PageBody_lbnNextPage'
     driver = webdriver.Firefox()
     driver.get(url)
     html = driver.page_source
     soup = BeautifulSoup(html)
-    db = parse_page(soup)     
-    while soup.find(id=re.compile(next_page)):          
+    db = parse_page(soup)
+    '''
+    while soup.find(id=re.compile(next_page)):
         driver.find_element_by_id(next_page).click()
         soup = BeautifulSoup(driver.page_source)
         db.extend(parse_page(soup))
+    '''
+    return db
     
+# ============================================================
+if __name__ == "__main__":
+    db = parse_webpage()
