4e0b6d8fa698a903c49636d196a5944f8b195a9c
Hugues Fontenelle <hugues.fontenelle@gmail.com>
2014-12-17 15:45:04 +0100

    [dbass] splice_predict

--- dbass_annot.py
+++ dbass_annot.py
@@ -7,6 +7,7 @@ Created on Tue Dec  2 13:54:06 2014
 
 import json, codecs
 from annotation.splice.splice_annotate import SpliceAnnotate
+from annotation.splice.splice_predict import SplicePredict, JsonSplice
 
 # ============================================================
 # filter the 577 entries to SNP only (505 remaining)
@@ -36,9 +37,10 @@ for counter in range(0, len(db1)): # counter to be able to restart at a later po
     pos = var['pos']
     ref = var['ref']
     alt = var['alt']
+    print "%s %s:%d%s>%s" % (db_entry['gene'],chrom, pos, ref, alt)
     splice = SpliceAnnotate(chrom, pos, ref, alt)
     splice.ID = db_entry['var_id']
-    splice.use_algo(use_SSFL=True, use_MaxEntScan=True)
+    splice.use_algo(use_SSFL=True, use_MaxEntScan=True, use_GeneSplicer=True, use_NNSplice=True, use_HSF=True)
     splice.score_splice_sites()
     splice.get_closest_authentic()
     splice.append_to_json(json_handle)
@@ -46,3 +48,28 @@ for counter in range(0, len(db1)): # counter to be able to restart at a later po
 json_handle.seek(-2, 2) # in order to remove the latest ",\n"
 json_handle.write('\n]\n')
 json_handle.close()
+
+
+# ============================================================
+# predict
+json_splice = JsonSplice('dbass5_all_annotated_snp_scored.json') 
+results = {}
+for counter in range(0, 300):
+    db_entry = json_splice.json_dict[counter]
+    chrom = db_entry['chrom'] 
+    pos = db_entry['pos']
+    alt = db_entry['alt']
+    ref = db_entry['ref']
+    ID = db_entry['ID']
+    predict = SplicePredict(chrom, pos, ref, alt)
+    predict.ID = ID
+    predict.strategy = 'Houdayer'
+    #predict.load_json(json_splice)
+    predict.score_annotate()
+    effect, comments = predict.predict()
+    print "[%s] chr%s:%d%s>%s : %s (%s)" % (ID ,chrom, pos, ref, alt, effect, ', '.join(comments))
+    results[ID] = effect
+
+from collections import Counter
+Counter(results.values())
+# 300 ID: 275 No effect, 25 New cryptic
