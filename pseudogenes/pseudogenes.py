728336c1b0e481a868e4f03dc78ae8d12a55722d
Hugues Fontenelle <hugues.fontenelle@gmail.com>
2015-03-05 13:54:47 +0100

    [pseudogene] blast

--- pseudogenes/pseudogenes.py
+++ pseudogenes/pseudogenes.py
@@ -15,6 +15,7 @@ email = 'hugues.fontenelle@medisin.uio.no'
 
 import csv, sys, argparse
 from Bio import Entrez, SeqIO 
+from Bio.Blast import NCBIWWW
 
 # ------------------------------------------------------------
 def get_transcripts(genepanel_filename):
@@ -53,6 +54,56 @@ def get_pseudogenesID(GeneID):
     return record['IdList']
 
 # ------------------------------------------------------------  
+def get_pseudogene(pseudogenesID):
+    handle = Entrez.efetch(db="gene", id=str(pseudogenesID), rettype='xml')
+    records = Entrez.read(handle)
+    record = records[0]
+    record['Entrezgene_locus']
+    
+    seq_interval = record['Entrezgene_locus'][0]['Gene-commentary_seqs'][0]['Seq-loc_int']['Seq-interval']
+    pseudo = {
+        'pos_from' : int(seq_interval['Seq-interval_from']),
+        'pos_to' : int(seq_interval['Seq-interval_to']),
+        'accession' : record['Entrezgene_locus'][0]['Gene-commentary_accession']
+        }
+    try:
+        Seq_id_gi = seq_interval['Seq-interval_id']['Seq-id']['Seq-id_gi']
+        pseudo['seq-id-gi'] = Seq_id_gi
+    except:
+        print 'No seq-id-gi for pseudogene %s' % pseudogenesID
+    return pseudo
+
+# ------------------------------------------------------------  
+def get_fasta(refseq_id, start, end):
+    Entrez.email = "hugues.fontenelle@medisin.uio.no"
+    handle = Entrez.efetch(db="nucleotide",
+                           id=refseq_id,
+                           rettype="fasta",
+                           strand=1,
+                           seq_start=start,
+                           seq_stop=end)
+    entrez_record = SeqIO.read(handle, "fasta")
+    handle.close()
+    fasta = str(entrez_record.seq)
+    return fasta
+
+
+# ------------------------------------------------------------  
+def run_blast(pseudo, filename="my_blast.xml"):
+    fasta = get_fasta(pseudo['accession'], pseudo['pos_from'], pseudo['pos_to'])
+    result_handle = NCBIWWW.qblast(program="blastn", database="GPIPE/9606/105/ref_top_level", sequence=fasta) # entrez_query="9606[taxid] AND grch37"
+    save_file = open(filename, "w")
+    save_file.write(result_handle.read())
+    save_file.close()
+    result_handle.close()
+    #TODO: rename file with pseudogene name
+     
+# ------------------------------------------------------------  
+def interstect_blast_gene(GeneID, filename="my_blast.xml"):
+    result_handle = open("my_blast.xml")  
+    # do stuffs
+    
+# ============================================================  
 def main():
     parser = argparse.ArgumentParser(description='Find pseudogenes ID from genepanel CSV file.')
     parser.add_argument('--csv', action='store', dest='genepanel_filename', required=True, help='input genepanel CSV filename')
@@ -67,14 +118,19 @@ def main():
             print '%s\t%s\t%s\t%s' % (transcript[0], transcript[1], GeneID, ', '.join(pseudogenesIDs))
         else:
             print '%s\t%s\t%s\t%d' % (transcript[0], transcript[1], GeneID)
-
-# ------------------------------------------------------------    
-def get_pseudogene_cds(GeneID):
-    handle = Entrez.efetch(db="gene", id=str(GeneID), rettype='xml')
-    record=Entrez.read(handle)
-
+            
 # ============================================================  
-if __name__ == "__main__":
+'''if __name__ == "__main__":
     sys.exit(main())
+'''    
+
+transcripts = get_transcripts(genepanel_filename)
+transcript=transcripts[6] # ZEB2
+GeneID = get_GeneID(transcript[0])
+pseudogenesIDs = get_pseudogenesID(GeneID)
+pseudo = get_pseudogene(pseudogenesIDs[0])
+run_blast(pseudo)
+interstect_blast_gene(GeneID)
+
     
     
